cb53c0ef0a9f1f122daf4dd09842019fb929c567
Christopher <84825400+cwakeley@users.noreply.github.com>
2023-05-06 15:16:31 -0700

    Create MBB Creating Triggers.sql

---
+++ MBB Creating Triggers.sql
@@ -0,0 +1,24 @@
+/*
+Create a trigger that will sync orders table with the order_items table each time a new order is created.
+
+*/
+
+CREATE TRIGGER insert_new_orders
+AFTER INSERT ON order_items
+FOR EACH ROW
+REPLACE INTO orders
+SELECT 
+	order_id,
+    MIN(created_at) AS created_at,
+    MIN(website_session_id) AS website_session_id,
+    SUM(CASE
+		WHEN is_primary_item = 1 THEN product_id
+        ELSE NULL
+        END) AS primary_product_id,
+	COUNT(order_item_id) AS items_purchase,
+    SUM(price_usd) as price_usd,
+    SUM(cogs_usd) as cogs_usd
+FROM order_items
+WHERE order_id = new.order_id
+GROUP BY 1
+ORDER BY 1;
